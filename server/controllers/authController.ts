7a8ac6224caa12ceceb656236dfb63bed6285ff7
muhammedalaa1 <muhammedalaa2213@gmail.com>
2023-09-10 23:12:28 +0300

    fix controller

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -47,7 +47,7 @@ export const Login = expressAsyncHandler(async (req, res) => {
 	const token = createToken(exist);
 	res.cookie(process.env.AUTH_COOKIE, token, {
 		httpOnly: true,
-		sameSite: "lax",
+		sameSite: "none",
 		path: "/",
 		secure: process.env.NODE_ENV === "production",
 		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
