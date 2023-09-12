67cd77d37e8f1ef1c0120f0eb122cd7990a01d4c
muhammedalaa1 <muhammedalaa2213@gmail.com>
2023-09-13 01:59:29 +0300

    t

--- server/middleware/errorhandler.ts
+++ server/middleware/errorhandler.ts
@@ -18,50 +18,8 @@ const errorhandler = (
 	console.log(err);
 
 	return res.status(500).json({
-		message: "UnExpected Error, try to shsmo.",
+		message: "UnExpected Error, try more.",
 	});
-
-	/*
-	// console.log(err);
-	const statusCode = res.statusCode ?? 500;
-	switch (statusCode) {
-		case 400:
-			return res.status(400).json({
-				title: "Validation failed",
-				message: err.message,
-				stackTrace: err.stack,
-			});
-		case 401:
-			return res.status(401).json({
-				title: "Not Authorized",
-				message: err.message,
-				stackTrace: err.stack,
-			});
-		case 403:
-			return res.status(403).json({
-				title: "Forbidden",
-				message: err.message,
-				stackTrace: err.stack,
-			});
-		case 404:
-			return res.status(404).json({
-				title: "Not Found",
-				message: err.message,
-				stackTrace: err.stack,
-			});
-
-		case 500:
-			return res.status(500).json({
-				title: "Internal Server Error",
-				message: err.message,
-				stackTrace: err.stack,
-			});
-
-		default:
-			console.log("No errors");
-			break;
-	}
-	res.json();*/
 };
 
 export default errorhandler;
