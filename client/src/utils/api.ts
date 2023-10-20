bbd2d4d837003dbd7316de42013b2dd4cbb7239e
muhammed <muhammedalaa2213@gmail.com>
2023-10-21 02:41:10 +0300

    T

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "http://localhost:5001",
+	baseURL: "http://https://book-stpre.onrender.com:5001",
 });
 
 //https://book-stpre.onrender.com
