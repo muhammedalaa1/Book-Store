5f7048511983c2f41694383ba2be94b67dec12b7
muhammedalaa1 <muhammedalaa2213@gmail.com>
2024-01-31 02:35:54 +0200

    test

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// export default axios.create({
+// 	baseURL: "http://localhost:5001",
+// });
 export default axios.create({
 	baseURL: "https://bookstore-303p.onrender.com",
 });
