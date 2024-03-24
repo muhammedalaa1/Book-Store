421d1517c24387e23e7142fa4d2159761c1f4e42
muhammedalaa1 <muhammedalaa2213@gmail.com>
2024-03-24 03:03:40 +0200

    host change

--- server/middleware/uploadImage.ts
+++ server/middleware/uploadImage.ts
@@ -6,49 +6,49 @@ dotenv.config();
 
 // Initialize ImageKit
 const imagekit = new ImageKit({
-	publicKey: process.env.PUBLIC_KEY,
-	privateKey: process.env.PRIVATE_KEY,
-	urlEndpoint: process.env.IMAGEKIT_ENDPOINT,
+    publicKey: process.env.PUBLIC_KEY,
+    privateKey: process.env.PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_ENDPOINT,
 });
 
 const upload = multer({
-	storage: multer.memoryStorage(),
-	limits: {
-		fileSize: 5 * 1024 * 1024, // no larger than 5mb
-	},
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // no larger than 5mb
+    },
 });
-
+//@ts-nocheck
 export function uploadSingleImage(req: Request, res, next) {
-	const uploadTask = upload.single("image");
-	console.log(req.files);
+    const uploadTask = upload.single("image");
+    console.log(req.files);
 
-	uploadTask(req, res, function (err) {
-		if (err) {
-			return res.send(err);
-		}
-		if (!req.file) {
-			if (req.params.bookId) {
-				return next();
-			}
-			res.status(400).send("No file uploaded.");
-			return;
-		}
+    uploadTask(req, res, function (err) {
+        if (err) {
+            return res.send(err);
+        }
+        if (!req.file) {
+            if (req.params.bookId) {
+                return next();
+            }
+            res.status(400).send("No file uploaded.");
+            return;
+        }
 
-		// Upload the image buffer to ImageKit
-		const image = {
-			fileName: req.file.originalname,
-			file: req.file.buffer.toString("base64"),
-		};
+        // Upload the image buffer to ImageKit
+        const image = {
+            fileName: req.file.originalname,
+            file: req.file.buffer.toString("base64"),
+        };
 
-		imagekit.upload(image, function (error, result) {
-			if (error) {
-				next(error);
-				return;
-			}
+        imagekit.upload(image, function (error, result) {
+            if (error) {
+                next(error);
+                return;
+            }
 
-			// Use the URL provided by ImageKit
-			req.body.image = result.url;
-			next();
-		});
-	});
+            // Use the URL provided by ImageKit
+            req.body.image = result.url;
+            next();
+        });
+    });
 }
